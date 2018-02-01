9d461cc13aaa686fe010a830dd9e00dd8eceec79
Krishan Wanarajan <kwanaraj@nyit.edu>
2018-02-01 01:13:42 -0500

    BugFix
    Fixed issue regarding tableview not loading

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
-    @IBOutlet weak var conditionLabel: UILabel!
-
+    
+    
+    @IBOutlet weak var tableStoresSizes: UITableView!
+    
+    
     var ref:DatabaseReference?
     var databasehandle:DatabaseHandle?
     
@@ -26,53 +29,72 @@ class ViewController: UIViewController {
     
     var parsedatabase = parseFirebase()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-            //Set the database reference
-            ref = Database.database().reference()
-        
-            //Gets Store Names from the Database
-            ref?.observeSingleEvent(of: .value, with: { (storesnapshot) in
-                for child in storesnapshot.childSnapshot(forPath: "Stores").children {
-                    let snap = child as! DataSnapshot
-                    let key = snap.key
-                    self.storeNames.append(key)
-                    
-                    
 
-                }
-               print(self.storeNames)
-            })
+        tableStoresSizes.delegate = self
+        tableStoresSizes.dataSource = self
         
+        fetchStoreData()
+        
+
+        //Set the database reference
+        ref = Database.database().reference()
+
             //Gets Clothing Sizes from the Database
             ref?.observeSingleEvent(of: .value, with: { (snapshot) in
                 for store in self.storeNames {
                     for child in snapshot.childSnapshot(forPath: "SizeGuide").childSnapshot(forPath: "\(store)").childSnapshot(forPath: "Mens").children{
                         let snap = child as! DataSnapshot
-                        let key = snap.key
-                        
-                                if (self.clothingSizes.contains("\(key)") ){
-                                    return
+                        let SizeKey = snap.key
+
+                        if (self.clothingSizes.contains("\(SizeKey)") ){
+                                    continue
                                 }
                                 else{
-                                    self.clothingSizes.append(key)
+                                    self.clothingSizes.append(SizeKey)
                                 }
                             }
-                       print(self.clothingSizes)
-                    
-                       print(self.parsedatabase.printCurrentData(stores: self.storeNames, sizes: self.clothingSizes))
                     }
-                
+                   // print(self.clothingSizes)
+                    print(self.parsedatabase.printCurrentData(stores: self.storeNames, sizes: self.clothingSizes))
                 })
-
         
         }
+
+  
+    func fetchStoreData() {
+        //Set the database reference
+         ref = Database.database().reference()
+       // Gets Store Names from the Database
+                    ref?.observeSingleEvent(of: .value, with: { (storesnapshot) in
+                        for child in storesnapshot.childSnapshot(forPath: "Stores").children {
+                            let snap = child as! DataSnapshot
+                            let StoreKey = snap.key
+                            self.storeNames.append(StoreKey)
+                            self.tableStoresSizes.reloadData()
+                        }
+                    })
+    }
+  
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return storeNames.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableStoresSizes.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel?.text = storeNames[indexPath.row]
+        
+        return cell!
+        
+    }
     
     func printDataToConsole() {
-
+        
         printDataToConsole()
     }
 
@@ -81,6 +103,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+   
 }
 
